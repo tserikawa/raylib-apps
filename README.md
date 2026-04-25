0d21d1fe4ba92fb391eae657041c1264cee9785f
tserikawa <tkt.srkw@gmail.com>
2026-04-25 09:08:43 +0900

    gitignore更新

--- README.md
+++
@@ -1,2 +0,0 @@
-# raylib-apps
-raylibで作ったデスクトップアプリ
--- lib/linear_algebra/solver.h
+++
@@ -1,9 +0,0 @@
-#ifndef ___solver
-#define ___solver
-
-#include "vector.h"
-#include "sqmatrix.h"
-
-void solver_gaussian_elimination(sqmatrix *a, vector *b, vector *x);
-
-#endif
